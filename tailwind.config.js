39afac5d7df76ef8f72f6cd2c8ac1d0c25d16fec
WaruiAlfred <alfredwarui17@gmail.com>
2023-11-17 02:32:46 +0300

    reconfigure tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      "Light-Cyan": "hsl(193, 38%, 86%)",
-      "Neon-Green": "hsl(150, 100%, 66%)",
-      "Grayish-Blue": "hsl(217, 19%, 38%)",
-      "Dark-Grayish-Blue": "hsl(217, 19%, 24%)",
-      "Dark-Blue": "hsl(218, 23%, 16%)",
-    },
     extend: {
+      colors: {
+        "Light-Cyan": "hsl(193, 38%, 86%)",
+        "Neon-Green": "hsl(150, 100%, 66%)",
+        "Grayish-Blue": "hsl(217, 19%, 38%)",
+        "Dark-Grayish-Blue": "hsl(217, 19%, 24%)",
+        "Dark-Blue": "hsl(218, 23%, 16%)",
+      },
       fontFamily: {
         sans: "Manrope, sans-serif",
       },
