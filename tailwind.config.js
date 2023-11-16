20c979788935ecdfa8a9901060b37a87eb4d44bd
WaruiAlfred <alfredwarui17@gmail.com>
2023-11-16 22:43:21 +0300

    configure custom colors and font tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      "Light-Cyan": "hsl(193, 38%, 86%)",
+      "Neon-Green": "hsl(150, 100%, 66%)",
+      "Grayish-Blue": "hsl(217, 19%, 38%)",
+      "Dark-Grayish-Blue": "hsl(217, 19%, 24%)",
+      "Dark-Blue": "hsl(218, 23%, 16%)",
+    },
+    extend: {
+      fontFamily: {
+        sans: "Manrope, sans-serif",
+      },
+    },
   },
   plugins: [],
 };
